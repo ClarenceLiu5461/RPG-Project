a9e007105e67a9f6c1380e132284bfb6921f824c
ClarenceLiu5461 <frank1213450@gmail.com>
2023-08-06 10:19:35 +0800

    Interacttion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public LayerMask movementMask;
+    public Interactable focus;
 
     PlayerMotor motor;
     Camera cam;
@@ -27,8 +28,8 @@ public class PlayerController : MonoBehaviour
             {
                 //Move player to what we hit
                 motor.MoveToPoint(hit.point);
-
                 //Stop focusing any objects
+                RemoveFocus();
             }
         }
         //Right mouse button click
@@ -36,11 +37,38 @@ public class PlayerController : MonoBehaviour
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            if (Physics.Raycast(ray, out hit, 100))
             {
-                //Check if we hit an interactable
-                //If we did setit as our focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
